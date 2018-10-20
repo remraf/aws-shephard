08a65e8519423c6365a812849781e295fdbb90a6
Tony Vo <4.tonyvo@gmail.com>
2018-10-20 07:10:54 +0000

    move disable_hyperthreading script to local s3 bucket

--- pre-install.sh
+++ pre-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download and install hyperthread disabling script
-wget -O /etc/init.d/disable_hyperthreading https://cfncluster-public-scripts.s3.amazonaws.com/disable_hyperthreading
+wget -O /etc/init.d/disable_hyperthreading https://s3.amazonaws.com/shephard-s3/disable_hyperthreading
 chmod a+x /etc/init.d/disable_hyperthreading
 chkconfig add /etc/init.d/disable_hyperthreading
 chkconfig --level 2345 disable_hyperthreading on
